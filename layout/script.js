1a04376892f303128270af34ccbbcb1f17cdfa92
Zac <a19860101@gmail.com>
2020-08-19 15:01:06 +0800

    20200819

--- layout/script.js
+++ layout/script.js
@@ -1,8 +1,16 @@
 
-window.onload = function(){
-    let navBtn = document.querySelector('.nav-toggleBtn');
-    console.log(navBtn);
-    navBtn.onclick = function(){
-        document.querySelector('.nav-collapse').classList.toggle('active');
-    }
-}
+// window.onload = function(){
+//     let navBtn = document.querySelector('.nav-toggleBtn');
+//     console.log(navBtn);
+//     navBtn.onclick = function(){
+//         document.querySelector('.nav-collapse').classList.toggle('active');
+//     }
+// }
+
+$(function(){
+    $('.nav-toggleBtn').click(function(){
+        // $('.nav-collapse').toggle(600);
+        // $('.nav-collapse').fadeToggle(600);
+        $('.nav-collapse').slideToggle(600);
+    })
+})
